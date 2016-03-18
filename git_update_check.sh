eeb7f0fc04b4aa601e756d75a3cec76c3c21241e
Amalia Viti <amalia.viti@gmail.com>
2016-03-17 23:24:35 -0400

    adds git update script for cron

---
+++ git_update_check.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# git_update_check.sh
+#
+# Checks git remote for updates. If there are updates, it pulls, installs
+# dependencies, rebuilds, and restarts app. To be run via cron periodically.
+#
+# Add the following to your crontab to run every 15 min:
+# */15 * * * * ~/mirror/git_update_check.sh
+#
+# Requirements: forever (https://github.com/foreverjs/forever)
+
+cd ~/mirror
+git fetch origin
+reslog=$(git log HEAD..origin/master --oneline)
+
+if [[ "${reslog}" != "" ]] ; then
+  echo "Pulling..."
+  git pull origin master
+  echo "Updating dependencies..."
+  npm install
+  echo "Building app..."
+  npm run build
+  echo "Restarting app..."
+  forever restart ~/mirror/app.js
+else
+  echo "Up-to-date."
+fi
