d5b0124ff0ea2a3fb467f8b137521162afe61dfa
Amalia Viti <amalia.viti@gmail.com>
2017-10-11 00:41:40 -0400

    mta: store subway times by route direction/id

--- js/widgets/subway/index.js
+++ js/widgets/subway/index.js
@@ -23,63 +23,52 @@ class Subway extends Widget {
     clearInterval(this._interval)
   }
 
-  getTimes = (stationId, feedId, direction, timeToWalk) => {
-    const schedule = (this.props[`${feedId}_${stationId}`] && this.props[`${feedId}_${stationId}`][direction].slice(0, 5)) || []
-    let leave
-    const times = schedule.map(t => {
-      const { arrivalTime, routeId } = t
-      const arrivalFromNow = utils.formatTime(arrivalTime)
-      const timeToLeave = arrivalFromNow.numMin - timeToWalk
-
-      if (arrivalFromNow.numMin && timeToLeave >= 0 && !leave) {
-        leave = {
-          msg: `Leave ${timeToLeave === 0 ? 'now' : `in ${timeToLeave} min`}`,
-          routeId,
-        }
-      }
-      return { routeId, arrivalTime, arrivalFromNow }
-    })
-
-    return { leave, times }
-  }
-
   renderContent() {
-    const { error } = this.props
-    if (_.isEmpty(config.stops) || error) {
+    const { error, trains } = this.props
+    if (_.isEmpty(config.stops) || !Object.keys(trains).length || error) {
       return null
     }
-
     return (
       <div>
         <h5>
           <i className="logo" /> Subway
         </h5>
-        {config.stops.map(({ stationId, feedId, direction, timeToWalk }) => {
-          const schedule = this.getTimes(stationId, feedId, direction, timeToWalk)
-          // if no times then don't show
-          if (_.isEmpty(schedule.leave)) {
-            return null
-          }
-
-          const { routeId, msg } = schedule.leave
-          return (
-            <SubwayLine
-              key={`${feedId}_${stationId}`}
-              routeId={routeId}
-              msg={msg}
-            />
-          )
-        })}
+        {Object.keys(trains).map(routeId => (
+          <SubwayLine
+            key={routeId}
+            routeId={routeId}
+            times={trains[routeId]['S']}
+          />
+        ))}
       </div>
     )
   }
 }
 
-const SubwayLine = ({ routeId, msg }) => (
-  <div className="line">
-    <div className={'subway subway-' + routeId}>{routeId}</div>
-    <div className="text">{msg}</div>
-  </div>
-)
+const SubwayLine = ({ routeId, times }) => {
+  let msg = null
+  if (times.length < 1) return null
+
+  const lineConfig = config.stops.find((stop) => times[0].stationId === stop.stationId)
+  const { timeToWalk } = lineConfig
+
+  times.map(t => {
+    const { arrivalTime } = t
+    const arrivalFromNow = utils.formatTime(arrivalTime)
+    const timeToLeave = arrivalFromNow.numMin - timeToWalk
+    if (arrivalFromNow.numMin && timeToLeave >= 0 && !msg) {
+      msg = timeToLeave === 0 ? 'now' : `in ${timeToLeave} min`
+    }
+  })
+
+  if (!msg) return null
+
+  return (
+    <div className="line">
+      <div className={'subway subway-' + routeId}>{routeId}</div>
+      <div className="text">Leave {msg}</div>
+    </div>
+  )
+}
 
 export default connect(state => state.subway)(Subway)
--- js/widgets/subway/reducer.js
+++ js/widgets/subway/reducer.js
@@ -4,15 +4,45 @@ import {
   ERROR_SUBWAY_TIMES,
 } from './actions'
 
+const initialRouteState = { N: [], S: [] }
+
 function subway(state = {
   error: null,
+  trains: {
+    'A': initialRouteState,
+    'B': initialRouteState,
+    'C': initialRouteState,
+    'D': initialRouteState,
+    '2': initialRouteState,
+    '3': initialRouteState,
+  },
 }, action) {
   switch (action.type) {
     case RECEIVE_SUBWAY_TIMES:
+      const newRouteSchedules = {}
+      const { stationId, times: { schedule } } = action
+
+      Object.keys(schedule[stationId]).map((direction) => {
+        const times = schedule[stationId][direction]
+        times.map((t) => {
+          const { routeId } = t
+          if (!newRouteSchedules[routeId]) {
+            newRouteSchedules[routeId] = { N: [], S: [] }
+          }
+          if (!newRouteSchedules[routeId][direction]) {
+            newRouteSchedules[routeId][direction] = []
+          }
+
+          newRouteSchedules[routeId][direction].push({ ...t, stationId })
+        })
+      })
+
       return {
         ...state,
-        [`${action.feedId}_${action.stationId}`]: action.times.schedule[action.stationId],
-        error: null,
+        trains: {
+          ...state.trains,
+          ...newRouteSchedules,
+        }
       }
     case ERROR_SUBWAY_TIMES:
       return {
